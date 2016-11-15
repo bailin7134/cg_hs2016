0e394c48b8ca191935d5493c3461165f30615be9
Lin Bai <lbai.ee.ethz@gmail.com>
2016-11-15 21:24:18 +0100

    update simple.java

--- hw4_2/src/main/java/simple/simple.java
+++ hw4_2/src/main/java/simple/simple.java
@@ -6,6 +6,8 @@ import jrtr.gldeferredrenderer.*;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.IOException;
+
 import javax.vecmath.*;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -21,6 +23,7 @@ public class simple
 	static Shader normalShader;
 	static Shader diffuseShader;
 	static Shader phongShader;
+	static ObjReader teapotReader;
 	static Material material;
 	static SimpleSceneManager sceneManager;
 	static Shape shape;
@@ -78,60 +81,60 @@ public class simple
 
 			// Construct a data structure that stores the vertices, their
 			// attributes, and the triangle mesh connectivity
-			VertexData vertexData = renderContext.makeVertexData(24);
-			vertexData.addElement(c, VertexData.Semantic.COLOR, 3);
-			vertexData.addElement(v, VertexData.Semantic.POSITION, 3);
-			vertexData.addElement(n, VertexData.Semantic.NORMAL, 3);
-			vertexData.addElement(uv, VertexData.Semantic.TEXCOORD, 2);
-			
-			// The triangles (three vertex indices for each triangle)
-			int indices[] = {0,2,3, 0,1,2,			// front face
-							 4,6,7, 4,5,6,			// left face
-							 8,10,11, 8,9,10,		// back face
-							 12,14,15, 12,13,14,	// right face
-							 16,18,19, 16,17,18,	// top face
-							 20,22,23, 20,21,22};	// bottom face
-
-			vertexData.addIndices(indices);
-								
+//			VertexData vertexData = renderContext.makeVertexData(24);
+//			vertexData.addElement(c, VertexData.Semantic.COLOR, 3);
+//			vertexData.addElement(v, VertexData.Semantic.POSITION, 3);
+//			vertexData.addElement(n, VertexData.Semantic.NORMAL, 3);
+//			vertexData.addElement(uv, VertexData.Semantic.TEXCOORD, 2);
+//			
+//			// The triangles (three vertex indices for each triangle)
+//			int indices[] = {0,2,3, 0,1,2,			// front face
+//							 4,6,7, 4,5,6,			// left face
+//							 8,10,11, 8,9,10,		// back face
+//							 12,14,15, 12,13,14,	// right face
+//							 16,18,19, 16,17,18,	// top face
+//							 20,22,23, 20,21,22};	// bottom face
+//
+//			vertexData.addIndices(indices);
 			// Make a scene manager and add the object
+//			sceneManager = new SimpleSceneManager();
+//			shape = new Shape(vertexData);
+//			sceneManager.addShape(shape);
+
+			//
+			VertexData vertexData;
+			teapotReader = new ObjReader();
+			try {
+				vertexData = teapotReader.read("../obj/teapot.obj", 5.0f, renderContext);
+			} catch (IOException e) {
+				vertexData = defaultShape();
+				System.out.print("Failed to read teapot!");
+				System.out.print(e.getMessage());
+			}
+
 			sceneManager = new SimpleSceneManager();
 			shape = new Shape(vertexData);
 			sceneManager.addShape(shape);
-
-			//
+			
 			Light l = new Light();
 			
-//			l.diffuse = new Vector3f(0f,0f,1f);
-//			l.direction = new Vector3f(0f,0f,1f);
-//			sceneManager.addLight(l);
-//			l = new Light();
-//			l.diffuse = new Vector3f(1f,0f,0f);
-//			l.direction = new Vector3f(0f,1f,0f);
-//			sceneManager.addLight(l);
-//			l = new Light();
 			l.diffuse = new Vector3f(1f,0f,0f);
-			l.position = new Vector3f(1f,1f,0f);
+			l.position = new Vector3f(1f,1f,3f);
 			l.type = Light.Type.POINT;
 			sceneManager.addLight(l);
 			l = new Light();
-			l.diffuse = new Vector3f(0f,1f,0f);
-			l.position = new Vector3f(-1f,-1f,0f);
+			l.diffuse = new Vector3f(1f,1f,0f);
+			l.position = new Vector3f(-2f,-3f,3f);
 			l.type = Light.Type.POINT;
 			sceneManager.addLight(l);
 			l = new Light();
-			l.diffuse = new Vector3f(0f,0f,1f);
-			l.position = new Vector3f(-1f,1f,0f);
-			l.type = Light.Type.POINT;
-			sceneManager.addLight(l);
-			l = new Light();
-			l.diffuse = new Vector3f(0f,0f,0f);
-			l.position = new Vector3f(1f,-1f,0f);
+			l.diffuse = new Vector3f(0f,1f,1f);
+			l.position = new Vector3f(-3f,2f,2f);
 			l.type = Light.Type.POINT;
 			sceneManager.addLight(l);
 //			l = new Light();
-//			l.position = new Vector3f(0.f,0.f,1.f);
-//			l.diffuse = new Vector3f(1.f,0.f,0.f);
+//			l.diffuse = new Vector3f(0f,0f,0f);
+//			l.position = new Vector3f(3f,-2f,2f);
 //			l.type = Light.Type.POINT;
 //			sceneManager.addLight(l);
 			
@@ -166,12 +169,12 @@ public class simple
 		    // Make a material that can be used for shading
 			material = new Material();
 			material.shader = phongShader;
-			material.diffuse = new Vector3f(1f, 0f, 0f);
+			material.diffuse = new Vector3f(0f, 1f, 0f);
 			material.specular = new Vector3f(1f, 1f, 1f);
 			material.shininess = 0.5f;
 			material.diffuseMap = renderContext.makeTexture();
 			try {
-				material.diffuseMap.load("../textures/wood.jpg");
+				material.diffuseMap.load("../textures/plant.jpg");
 			} catch(Exception e) {				
 				System.out.print("Could not load texture.\n");
 				System.out.print(e.getMessage());
@@ -196,6 +199,76 @@ public class simple
 		    currentstep = basicstep;
 		    timer.scheduleAtFixedRate(new AnimationTask(), 0, 10);
 		}
+		
+		private VertexData defaultShape(){
+			// A house
+			float vertices[] = {-4,-4,4, 4,-4,4, 4,4,4, -4,4,4,		// front face
+								-4,-4,-4, -4,-4,4, -4,4,4, -4,4,-4, // left face
+								4,-4,-4,-4,-4,-4, -4,4,-4, 4,4,-4,  // back face
+								4,-4,4, 4,-4,-4, 4,4,-4, 4,4,4,		// right face
+								4,4,4, 4,4,-4, -4,4,-4, -4,4,4,		// top face
+								-4,-4,4, -4,-4,-4, 4,-4,-4, 4,-4,4, // bottom face
+		
+								-20,-4,20, 20,-4,20, 20,-4,-20, -20,-4,-20, // ground floor
+								-4,4,4, 4,4,4, 0,8,4,				// the roof
+								4,4,4, 4,4,-4, 0,8,-4, 0,8,4,
+								-4,4,4, 0,8,4, 0,8,-4, -4,4,-4,
+								4,4,-4, -4,4,-4, 0,8,-4};
+		
+			float normals[] = {0,0,1,  0,0,1,  0,0,1,  0,0,1,		// front face
+							   -1,0,0, -1,0,0, -1,0,0, -1,0,0,		// left face
+							   0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,		// back face
+							   1,0,0,  1,0,0,  1,0,0,  1,0,0,		// right face
+							   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// top face
+							   0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,		// bottom face
+		
+							   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// ground floor
+							   0,0,1,  0,0,1,  0,0,1,				// front roof
+							   0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, // right roof
+							   -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, // left roof
+							   0,0,-1, 0,0,-1, 0,0,-1};				// back roof
+							   
+			float colors[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
+							  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+							  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+							  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+							  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+							  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+			
+							  0,0.5f,0, 0,0.5f,0, 0,0.5f,0, 0,0.5f,0,			// ground floor
+							  0,0,1, 0,0,1, 0,0,1,							// roof
+							  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+							  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+							  0,0,1, 0,0,1, 0,0,1,};
+		
+			// Set up the vertex data
+			VertexData vertexData = renderContext.makeVertexData(42);
+		
+			// Specify the elements of the vertex data:
+			// - one element for vertex positions
+			vertexData.addElement(vertices, VertexData.Semantic.POSITION, 3);
+			// - one element for vertex colors
+			vertexData.addElement(colors, VertexData.Semantic.COLOR, 3);
+			// - one element for vertex normals
+			vertexData.addElement(normals, VertexData.Semantic.NORMAL, 3);
+			
+			// The index data that stores the connectivity of the triangles
+			int indices[] = {0,2,3, 0,1,2,			// front face
+						 	4,6,7, 4,5,6,			// left face
+						 	8,10,11, 8,9,10,		// back face
+						 	12,14,15, 12,13,14,	// right face
+							16,18,19, 16,17,18,	// top face
+							20,22,23, 20,21,22,	// bottom face
+				                
+							24,26,27, 24,25,26,	// ground floor
+							28,29,30,				// roof
+							31,33,34, 31,32,33,
+							35,37,38, 35,36,37,
+							39,40,41};	
+			
+			vertexData.addIndices(indices);
+			return vertexData;
+		}
 	}
 
 	/**
@@ -212,8 +285,8 @@ public class simple
     		rotX.rotX(currentstep);
     		Matrix4f rotY = new Matrix4f();
     		rotY.rotY(currentstep);
-    		t.mul(rotX);
-    		t.mul(rotY);
+//    		t.mul(rotX);
+//    		t.mul(rotY);
     		shape.setTransformation(t);
     		
     		// Trigger redrawing of the render window
--- hw4_3/src/main/java/simple/simple.java
+++ hw4_3/src/main/java/simple/simple.java
@@ -25,6 +25,7 @@ public class simple
 	static SimpleSceneManager sceneManager;
 	static Shape shape;
 	static float currentstep, basicstep;
+	static Shape house;
 
 	/**
 	 * An extension of {@link GLRenderPanel} or {@link SWRenderPanel} to 
@@ -98,21 +99,44 @@ public class simple
 			sceneManager = new SimpleSceneManager();
 			shape = new Shape(vertexData);
 			sceneManager.addShape(shape);
+			
+			house = makeHouse(renderContext);
+			sceneManager.addShape(house);
 
 			//
 			Light l = new Light();
 
+//			l.diffuse = new Vector3f(1f,0f,0f);
+//			l.direction = new Vector3f(0f,0f,-8f);
+//			//sceneManager.addLight(l);
+//			l = new Light();
+//			l.diffuse = new Vector3f(1f,0f,0f);
+//			l.direction = new Vector3f(-1f,-1f,-1f);
+//			//sceneManager.addLight(l);
+//			l = new Light();
+//			l.diffuse = new Vector3f(1f,0f,0f);
+//			l.specular = new Vector3f(1f,0f,0f);
+//			l.position = new Vector3f(1f,1f,-1f);
+//			l.type = Light.Type.POINT;
+//			sceneManager.addLight(l);
+			
 			l.diffuse = new Vector3f(1f,0f,0f);
-			l.direction = new Vector3f(0f,0f,-8f);
-			//sceneManager.addLight(l);
+			l.position = new Vector3f(1f,1f,1f);
+			l.type = Light.Type.POINT;
+			sceneManager.addLight(l);
 			l = new Light();
-			l.diffuse = new Vector3f(1f,0f,0f);
-			l.direction = new Vector3f(-1f,-1f,-1f);
-			//sceneManager.addLight(l);
+			l.diffuse = new Vector3f(0f,1f,0f);
+			l.position = new Vector3f(-1f,-1f,-1f);
+			l.type = Light.Type.POINT;
+			sceneManager.addLight(l);
 			l = new Light();
-			l.diffuse = new Vector3f(1f,0f,0f);
-			l.specular = new Vector3f(1f,0f,0f);
-			l.position = new Vector3f(1f,1f,-1f);
+			l.diffuse = new Vector3f(0f,0f,1f);
+			l.position = new Vector3f(-1f,1f,0f);
+			l.type = Light.Type.POINT;
+			sceneManager.addLight(l);
+			l = new Light();
+			l.diffuse = new Vector3f(0f,0f,0f);
+			l.position = new Vector3f(1f,-1f,0f);
 			l.type = Light.Type.POINT;
 			sceneManager.addLight(l);
 			
@@ -147,7 +171,7 @@ public class simple
 		    // Make a material that can be used for shading
 			material = new Material();
 			material.shader = phongShader;
-			material.diffuse = new Vector3f(1f, 0f, 0f);
+			material.diffuse = new Vector3f(1f, 1f, 1f);
 			material.specular = new Vector3f(1f, 1f, 1f);
 			material.shininess = 0.5f;
 			material.diffuseMap = renderContext.makeTexture();
@@ -275,6 +299,47 @@ public class simple
 					}
 					break;
 				}
+				/*
+				 *  add parameters for Frustum and Camera
+				 *  image 1
+				 *  F: aspect ration 1, 
+				 *     vertical field of view 60, 
+				 *     near far clip plans 1,100
+				 *  C: center of projection 0,0,40
+				 *     look at point 0,0,0
+				 *     up vector 0,1,0
+				 */
+				case 'h': {
+					sceneManager.getFrustum().setNear(1);
+					sceneManager.getFrustum().setFar(100);
+					sceneManager.getFrustum().setAspect(1);
+					sceneManager.getFrustum().getFovGrad(60);
+					sceneManager.getCamera().setCenterOfProjection(new Vector3f(0f,0f,40f));
+					sceneManager.getCamera().setLookAtPoint(new Vector3f(0f,0f,0f));
+					sceneManager.getCamera().setUpVector(new Vector3f(0f,1f,0f));
+					break;
+				}
+				
+				/*
+				 *  add parameters for Frustum and Camera
+				 *  image 2
+				 *  F: aspect ration 1, 
+				 *     vertical field of view 60, 
+				 *     near far clip plans 1,100
+				 *  C: center of projection --10,40,40
+				 *     look at point -5,0,0
+				 *     up vector 0,1,0
+				 */
+				case 'v': {
+					sceneManager.getFrustum().setNear(1);
+					sceneManager.getFrustum().setFar(100);
+					sceneManager.getFrustum().setAspect(1);
+					sceneManager.getFrustum().getFovGrad(60);
+					sceneManager.getCamera().setCenterOfProjection(new Vector3f(-10f,40f,40f));
+					sceneManager.getCamera().setLookAtPoint(new Vector3f(-5f,0f,0f));
+					sceneManager.getCamera().setUpVector(new Vector3f(0f,1f,0f));
+					break;
+				}
 			}
 			
 			// Trigger redrawing
@@ -316,5 +381,80 @@ public class simple
 	    
 	    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    jframe.setVisible(true); // show window
+	}
+
+
+	public static Shape makeHouse(RenderContext renderContext)
+	{
+		// A house
+		float vertices[] = {-4,-4,4, 4,-4,4, 4,4,4, -4,4,4,		// front face
+							-4,-4,-4, -4,-4,4, -4,4,4, -4,4,-4, // left face
+							4,-4,-4,-4,-4,-4, -4,4,-4, 4,4,-4,  // back face
+							4,-4,4, 4,-4,-4, 4,4,-4, 4,4,4,		// right face
+							4,4,4, 4,4,-4, -4,4,-4, -4,4,4,		// top face
+							-4,-4,4, -4,-4,-4, 4,-4,-4, 4,-4,4, // bottom face
+	
+							-20,-4,20, 20,-4,20, 20,-4,-20, -20,-4,-20, // ground floor
+							-4,4,4, 4,4,4, 0,8,4,				// the roof
+							4,4,4, 4,4,-4, 0,8,-4, 0,8,4,
+							-4,4,4, 0,8,4, 0,8,-4, -4,4,-4,
+							4,4,-4, -4,4,-4, 0,8,-4};
+	
+		float normals[] = {0,0,1,  0,0,1,  0,0,1,  0,0,1,		// front face
+						   -1,0,0, -1,0,0, -1,0,0, -1,0,0,		// left face
+						   0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,		// back face
+						   1,0,0,  1,0,0,  1,0,0,  1,0,0,		// right face
+						   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// top face
+						   0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0,		// bottom face
+	
+						   0,1,0,  0,1,0,  0,1,0,  0,1,0,		// ground floor
+						   0,0,1,  0,0,1,  0,0,1,				// front roof
+						   0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, 0.707f,0.707f,0, // right roof
+						   -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, -0.707f,0.707f,0, // left roof
+						   0,0,-1, 0,0,-1, 0,0,-1};				// back roof
+						   
+		float colors[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
+						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+						  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+						  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+						  0,0,1, 0,0,1, 0,0,1, 0,0,1,
+		
+						  0,0.5f,0, 0,0.5f,0, 0,0.5f,0, 0,0.5f,0,			// ground floor
+						  0,0,1, 0,0,1, 0,0,1,							// roof
+						  1,0,0, 1,0,0, 1,0,0, 1,0,0,
+						  0,1,0, 0,1,0, 0,1,0, 0,1,0,
+						  0,0,1, 0,0,1, 0,0,1,};
+	
+		// Set up the vertex data
+		VertexData vertexData = renderContext.makeVertexData(42);
+	
+		// Specify the elements of the vertex data:
+		// - one element for vertex positions
+		vertexData.addElement(vertices, VertexData.Semantic.POSITION, 3);
+		// - one element for vertex colors
+		vertexData.addElement(colors, VertexData.Semantic.COLOR, 3);
+		// - one element for vertex normals
+		vertexData.addElement(normals, VertexData.Semantic.NORMAL, 3);
+		
+		// The index data that stores the connectivity of the triangles
+		int indices[] = {0,2,3, 0,1,2,			// front face
+						 4,6,7, 4,5,6,			// left face
+						 8,10,11, 8,9,10,		// back face
+						 12,14,15, 12,13,14,	// right face
+						 16,18,19, 16,17,18,	// top face
+						 20,22,23, 20,21,22,	// bottom face
+		                 
+						 24,26,27, 24,25,26,	// ground floor
+						 28,29,30,				// roof
+						 31,33,34, 31,32,33,
+						 35,37,38, 35,36,37,
+						 39,40,41};	
+	
+		vertexData.addIndices(indices);
+	
+		Shape house = new Shape(vertexData);
+		
+		return house;
 	}
 }
