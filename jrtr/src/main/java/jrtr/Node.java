4d9739de2acd2bad6880595bd8b6e328a88bb80a
Lin Bai <lbai.ee.ethz@gmail.com>
2016-11-20 18:21:14 +0100

    add Node

---
+++ jrtr/src/main/java/jrtr/Node.java
@@ -0,0 +1,12 @@
+package jrtr;
+
+import java.util.LinkedList;
+import javax.vecmath.Matrix4f;
+
+public interface Node {
+	
+	public Matrix4f getTransformation();
+	public Shape getShape();
+	public LinkedList<Node> getChildren();
+	
+}
